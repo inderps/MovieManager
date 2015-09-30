149a1852df0bd81dacc3f008a18a0675011b5064
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-09-30 14:39:01 +0200

    added local storage

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
-// Vendors
 window.$ = window.jQuery = require('jquery');
 require('angular');
 require('angular-route');
 require('angular-resource');
+require('angular-local-storage');
 require("bootstrap-webpack");
 
-
-//Angular Module Definitions and Config including routes
-angular.module('MovieManager', ['ngRoute', 'ngResource']).config(function($routeProvider){
+angular.module('MovieManager', ['ngRoute', 'ngResource', 'LocalStorageModule'])
+  .config(function($routeProvider, localStorageServiceProvider){
+    localStorageServiceProvider
+      .setPrefix('MovieManager')
+      .setStorageType('sessionStorage')
+      .setNotify(true, true);
 
 });
